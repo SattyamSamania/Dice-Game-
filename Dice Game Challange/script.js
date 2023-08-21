7e42ce41479f5cdbdd6cb480ce598eab25e56272
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-08-21 19:34:27 +0530

    Add files via upload

---
+++ Dice Game Challange/script.js
@@ -0,0 +1,28 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;  //This will give the random number from 1 to 6
+
+var randomDiceImage = "dice" + randomNumber + ".png";  // dice1.png to dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png to images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+
+// If Player 1 wins
+
+if(randomNumber> randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!"
+}
+ else if(randomNumber2> randomNumber){
+    document.querySelector("h1").innerHTML = "Player 2 wins!"
+}
+else{
+    document.querySelector("h1").innerHTML = "It's Draw!"
+}
